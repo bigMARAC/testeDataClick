fd348c628e3bfeb124dab7e0e6347345c32e8829
Marcos Emanuel <45175801+bigMARAC@users.noreply.github.com>
2021-02-03 20:34:29 -0300

    Questao 01

--- Questao01/main.js
+++ Questao01/main.js
@@ -1,10 +1,10 @@
 for (let index = 1; index <= 100; index++) {
     if (index % 3 === 0 && index % 5 === 0) {
-        console.log("FizzBuzz: " + index)
+        console.log("FizzBuzz")
     } else if (index % 3 === 0) {
-        console.log("Fizz: " + index)
+        console.log("Fizz")
     } else if (index % 5 === 0) {
-        console.log("Buzz: " + index)
+        console.log("Buzz")
     } else {
         console.log(index)
     }
